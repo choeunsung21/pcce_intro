d50fad52d598bfe41e4d0e88b0c1325a7884ef2f
choeunsung21 <eesvcho@gmail.com>
2024-12-03 09:16:32 +0900

    commit 프로그래머스
    	-day01

--- pcce_intro/src/day01/Run.java
+++ pcce_intro/src/day01/Run.java
@@ -1,10 +1,10 @@
 package day01;
-
 public class Run {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Solution01 s1 = new Solution01();
+		int n1 = s1.solution(2, 3);
+		System.out.println("2,3일 때 : " + n1);
+		int n2 = s1.solution(100, 2);
+		System.out.println("100,2일 때 : " + n2);
 	}
-
 }
